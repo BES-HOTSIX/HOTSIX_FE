8b9347115f4d047e67a73f4b3d0c401186d23ae2
bhj2bb <cs2uee@gmail.com>
2024-01-30 17:30:51 +0900

    feat : 쇼셜로그인 UI개선

--- src/components/auth/SignInForm.js
+++ src/components/auth/SignInForm.js
@@ -1,35 +1,35 @@
-'use client'
+'use client';
 
-import React, { useState } from 'react'
-import { Input, Button } from '@nextui-org/react'
-import { FaUser, FaLock } from 'react-icons/fa'
-import { useLoginUser } from '@/hooks/useUser'
-import KakaoLogin from './KakaoLogin'
-import { GoogleLogin } from './GoogleLogin'
-import NaverLogin from './NaverLogin'
+import React, { useState } from 'react';
+import { Input, Button } from '@nextui-org/react';
+import { FaUser, FaLock } from 'react-icons/fa';
+import { useLoginUser } from '@/hooks/useUser';
+import KakaoLogin from './KakaoLogin';
+import { GoogleLogin } from './GoogleLogin';
+import NaverLogin from './NaverLogin';
 
 export default function SignInForm() {
-  const { submitLoginUser, isPending, isError, error } = useLoginUser()
+  const { submitLoginUser, isPending, isError, error } = useLoginUser();
 
   const [loginForm, setLoginForm] = useState({
     username: '',
     password: '',
-  })
+  });
 
   const handleChange = (e) => {
-    e.preventDefault()
-    const { name, value } = e.target
+    e.preventDefault();
+    const { name, value } = e.target;
     setLoginForm({
       ...loginForm,
       [name]: value,
-    })
-  }
+    });
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    submitLoginUser(loginForm)
-  }
+    submitLoginUser(loginForm);
+  };
 
   return (
     <div className='flex flex-col items-center justify-center h-[70vh]'>
@@ -70,10 +70,31 @@ export default function SignInForm() {
           로그인
         </Button>
 
-        <KakaoLogin />
-        <NaverLogin />
-        <GoogleLogin />
+        {/* 쇼셜 로그인 구분선 */}
+        <div className='w-full my-4 flex items-center'>
+          <div className='border-t border-gray-300 flex-grow'></div>
+          <p className='mx-2 text-sm text-gray-600'>쇼셜 로그인</p>
+          <div className='border-t border-gray-300 flex-grow'></div>
+        </div>
+
+        {/* 쇼셜 로그인 버튼과 문구 */}
+        <div className='flex flex-col items-center'>
+          <div className='flex flex-col gap-4'>
+            <div className='hover:bg-gray-100 p-2 rounded-md cursor-pointer flex items-center justify-center transition-all duration-300 transform hover:border'>
+              <span>카카오 로그인</span>
+              <KakaoLogin />
+            </div>
+            <div className='hover:bg-gray-100 p-2 rounded-md cursor-pointer flex items-center justify-center transition-all duration-300 transform hover:border'>
+              <span>네이버 로그인</span>
+              <NaverLogin />
+            </div>
+            <div className='hover:bg-gray-100 p-2 rounded-md cursor-pointer flex items-center justify-center transition-all duration-300 transform hover:border'>
+              <span>구글 로그인</span>
+              <GoogleLogin />
+            </div>
+          </div>
+        </div>
       </form>
     </div>
-  )
+  );
 }
