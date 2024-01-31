c2c78e3b3e3f7900a5d534f8660d195690ee6319
jkeum-dev <jon9eun@gmail.com>
2024-01-31 14:13:50 +0900

    feat : 멤버 관련해서 접근 권한 설정

--- src/components/reservation/HotelReservation.js
+++ src/components/reservation/HotelReservation.js
@@ -6,9 +6,10 @@ import { Button, Spacer, Input, Select } from '@nextui-org/react';
 import { useRouter } from 'next/navigation';
 import 'react-datepicker/dist/react-datepicker.css';
 import CalendarCustom from './Calendar';
+import axios from '@/config/axios-config'
 
 export default function HotelReservation({ id }) {
-	const { hotel, isHotelLoading, isError, error } = useHotelDetail(id)
+	const { hotel, isHotelLoading, isError, error } = useHotelDetail(id);
 	const [guestCount, setGuestCount] = useState(1);
 
 	// CalendarCustom에 props로 전달
@@ -63,22 +64,33 @@ export default function HotelReservation({ id }) {
 		};
 	
 		try {
-			const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/reserve/${id}`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify(reservationInfo)
+			const response = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/reserve/${id}`, reservationInfo, {
+				...axios.defaults,
+				useAuth: true,
 			});
+			// const token = sessionStorage.getItem('ACCESS_TOKEN_KEY');
+			// const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/reserve/${id}`, {
+			// 	method: 'POST',
+			// 	headers: {
+			// 		'Content-Type': 'application/json',
+			// 		'Authorization': `Bearer ${token}` // Bearer 토큰을 헤더에 추가
+			// 	},
+			// 	body: JSON.stringify(reservationInfo), // 요청 본문에 데이터 추가
+			// });
 
-			if (!response.ok) {
+			console.log('response: ', response);
+			if (response.status >= 400) {
 				throw new Error('Network response was not ok');
 			}
+			// if (!response.ok) {
+			// 	throw new Error('Network response was not ok');
+			// }
 
-			const result = await response.json();
-			console.log('Reservation successful:', result);
 			// Redirect or show success message
-			const reserveId = result.objData.id;
+			const reserveId = response.data.objData.id;
+			// const result = await response.json();
+			// const reserveId = result.objData.id;
+			console.log('reserveId: ', reserveId);
 			router.push(`/cashLog/payByCash/${reserveId}`);
 		} catch (error) {
 			console.error('Error making reservation:', error);
