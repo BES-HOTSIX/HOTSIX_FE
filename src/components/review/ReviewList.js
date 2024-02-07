6bdfe9085da0fbd325786418e3dd900aefe38a08
leeyuhyun0104 <dldbgus0104@gmail.com>
2024-02-07 13:17:20 +0900

    design: 리뷰 디자인 수정

--- src/components/review/ReviewList.js
+++ src/components/review/ReviewList.js
@@ -10,12 +10,13 @@ import {
   ModalFooter,
 } from "@nextui-org/react"
 import EditReviewForm from "./EditReviewForm"
+import { size } from "lodash"
 
 const ReviewList = ({ hotelId, onReviewEdit }) => {
   const [recentReviews, setRecentReviews] = useState([])
   const [allReviews, setAllReviews] = useState([])
   const [showModal, setShowModal] = useState(false)
-  const [modalSize, setModalSize] = useState("md")
+  const [modalSize, setModalSize] = useState()
   const [editingReviewId, setEditingReviewId] = useState(null)
   const [isEditing, setIsEditing] = useState(false)
 
@@ -66,14 +67,11 @@ const ReviewList = ({ hotelId, onReviewEdit }) => {
   }
 
   const handleDeleteReview = async (id) => {
-    console.log("Review ID:", id)
-
     try {
       await axios.delete(
         `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/review/delete/${id}`
       )
       console.log("리뷰가 성공적으로 삭제되었습니다.")
-      // 삭제 후 리뷰 다시 로드
       fetchReviews()
       alert("리뷰가 성공적으로 삭제되었습니다.")
     } catch (error) {
@@ -94,22 +92,23 @@ const ReviewList = ({ hotelId, onReviewEdit }) => {
     }
 
     return recentReviews.map((review) => (
-      <div key={review.id} style={{ flex: 1, padding: "10px" }}>
-        <p>리뷰번호: {review.id}</p>
-        <p>회원명: {review.member}</p>
-        <p>
+      <div key={review.id}>
+        <p style={{ margin: "10px" }}>회원명: {review.member}</p>
+        <p style={{ margin: "10px" }}>
           평점: {renderStars(review.rating)} ({review.rating})
         </p>
-        <p style={{ textAlign: "left", whiteSpace: "pre-line" }}>
+        <p
+          style={{ textAlign: "left", whiteSpace: "pre-line", margin: "10px" }}
+        >
           {review.body}
         </p>
-        <p>
+        <p style={{ margin: "10px" }}>
           편의시설: {renderStars(review.amenities)} ({review.amenities})
         </p>
-        <p>
+        <p style={{ margin: "10px" }}>
           서비스: {renderStars(review.staffService)} ({review.staffService})
         </p>
-        <p>
+        <p style={{ margin: "10px" }}>
           청결도: {renderStars(review.cleanliness)} ({review.cleanliness})
         </p>
         <Button
@@ -130,123 +129,142 @@ const ReviewList = ({ hotelId, onReviewEdit }) => {
 
   return (
     <div style={{ textAlign: "left" }}>
-      <h2 className='mb-3'>최근 리뷰</h2>
-      {renderRecentReviewsGrid()}
-      {recentReviews.length > 0 && allReviews.length > 0 && (
-        <>
-          {isEditing ? (
-            // EditReviewForm을 렌더링하도록 변경
-            <EditReviewForm
-              reviewId={editingReviewId}
-              onClose={() => setIsEditing(false)}
-              style={{ width: "300px" }} // 수정이 완료되면 isEditing을 false로 설정
-            />
-          ) : (
-            <Button onClick={() => handleShowModal("xl")}>
-              전체 리뷰 보기
-            </Button>
-          )}
-        </>
-      )}
-      <Modal
-        isOpen={showModal}
-        onOpenChange={handleCloseModal}
-        size={modalSize}
-        placement='auto'
-      >
-        <ModalContent
-          style={{
-            top: "10vh",
-            maxHeight: "80vh",
-            overflowY: "auto",
-            maxWidth: "80vw",
-            width: "50%",
-          }}
+      <h2 className='text-xl font-semibold mb-4 mt-5'>이용 후기</h2>
+      <div className='w-[55vw]'>
+        <div className='w-[40vw]'>
+          <div className='border-t-2 border-gray-200 mt-4 pt-4'></div>
+        </div>
+
+        {renderRecentReviewsGrid()}
+        {recentReviews.length > 0 && allReviews.length > 0 && (
+          <>
+            {isEditing ? (
+              // EditReviewForm을 렌더링하도록 변경
+              <EditReviewForm
+                reviewId={editingReviewId}
+                onClose={() => setIsEditing(false)} // 수정이 완료되면 isEditing을 false로 설정
+              />
+            ) : (
+              <Button onClick={() => handleShowModal("xl")}>
+                전체 리뷰 보기
+              </Button>
+            )}
+          </>
+        )}
+        <Modal
+          isOpen={showModal}
+          onOpenChange={handleCloseModal}
+          size={modalSize}
+          placement='auto'
         >
-          {(onClose) => (
-            <>
-              {editingReviewId !== null ? ( // 수정 폼 렌더링을 위한 조건 추가
-                <EditReviewForm
-                  hotelId={hotelId}
-                  reviewId={editingReviewId}
-                  onClose={() => {
-                    // 수정 폼에서 닫기 버튼을 눌렀을 때 호출되는 함수
-                    handleCloseModal()
-                    // 추가적인 처리가 필요하다면 onReviewEdit를 호출하여 처리할 수 있음
-                    if (onReviewEdit) {
-                      onReviewEdit()
-                    }
-                  }}
-                />
-              ) : (
-                <>
-                  <ModalHeader className='flex flex-col gap-1'>
-                    전체 리뷰
-                  </ModalHeader>
-                  <ModalBody>
-                    {Array.isArray(allReviews) && allReviews.length > 0 ? (
-                      <ul style={{ listStyleType: "none", padding: 0 }}>
-                        {allReviews.map((review) => (
-                          <li key={review.id} style={{ marginBottom: "20px" }}>
-                            <p>리뷰번호: {review.id}</p>
-                            <p>회원명: {review.member}</p>
-                            <p>
-                              평점: {renderStars(review.rating)} (
-                              {review.rating})
-                            </p>
-                            <p>{review.body}</p>
-                            <p>
-                              편의시설: {renderStars(review.amenities)} (
-                              {review.amenities})
-                            </p>
-                            <p>
-                              서비스: {renderStars(review.staffService)} (
-                              {review.staffService})
-                            </p>
-                            <p>
-                              청결도: {renderStars(review.cleanliness)} (
-                              {review.cleanliness})
-                            </p>
-                            <Button
-                              onClick={() => handleEditReview(review.id)}
-                              style={{
-                                backgroundColor: "orange",
-                                color: "white",
-                              }}
-                            >
-                              수정
-                            </Button>
-                            <Button
-                              onClick={() => handleDeleteReview(review.id)}
-                              style={{ backgroundColor: "red", color: "white" }}
-                            >
-                              삭제
-                            </Button>
-                          </li>
-                        ))}
-                      </ul>
-                    ) : (
-                      <p>리뷰가 없습니다</p>
-                    )}
-                  </ModalBody>
-                  <ModalFooter>
-                    <Button
-                      onPress={() => {
-                        if (onReviewEdit) {
-                          onReviewEdit()
-                        }
+          <ModalContent
+            style={{
+              top: "10vh",
+              maxHeight: "80vh",
+              overflowY: "auto",
+              maxWidth: "80vw",
+              width: "50%",
+            }}
+          >
+            {(onClose) => (
+              <>
+                {editingReviewId !== null ? ( // 수정 폼 렌더링을 위한 조건 추가
+                  <EditReviewForm
+                    hotelId={hotelId}
+                    reviewId={editingReviewId}
+                    onClose={() => {
+                      // 수정 폼에서 닫기 버튼을 눌렀을 때 호출되는 함수
+                      handleCloseModal()
+                      // 추가적인 처리가 필요하다면 onReviewEdit를 호출하여 처리할 수 있음
+                      if (onReviewEdit) {
+                        onReviewEdit()
+                      }
+                    }}
+                  />
+                ) : (
+                  <>
+                    <h4
+                      style={{
+                        margin: "30px",
+                        fontSize: "25px",
                       }}
-                      style={{ backgroundColor: "#007bff", color: "#ffffff" }}
                     >
-                      닫기
-                    </Button>
-                  </ModalFooter>
-                </>
-              )}
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+                      전체 리뷰
+                    </h4>
+                    <ModalBody>
+                      {Array.isArray(allReviews) && allReviews.length > 0 ? (
+                        <ul
+                          style={{
+                            listStyleType: "none",
+                            paddingLeft: "40px",
+                            paddingRight: "40px",
+                          }}
+                        >
+                          {allReviews.map((review) => (
+                            <li key={review.id}>
+                              <p style={{ margin: "10px" }}>
+                                회원명: {review.member}
+                              </p>
+                              <p style={{ margin: "10px" }}>
+                                평점: {renderStars(review.rating)} (
+                                {review.rating})
+                              </p>
+                              <p style={{ margin: "10px" }}>{review.body}</p>
+                              <p style={{ margin: "10px" }}>
+                                편의시설: {renderStars(review.amenities)} (
+                                {review.amenities})
+                              </p>
+                              <p style={{ margin: "10px" }}>
+                                서비스: {renderStars(review.staffService)} (
+                                {review.staffService})
+                              </p>
+                              <p style={{ margin: "10px" }}>
+                                청결도: {renderStars(review.cleanliness)} (
+                                {review.cleanliness})
+                              </p>
+                              <Button
+                                onClick={() => handleEditReview(review.id)}
+                                style={{
+                                  backgroundColor: "orange",
+                                  color: "white",
+                                }}
+                              >
+                                수정
+                              </Button>
+                              <Button
+                                onClick={() => handleDeleteReview(review.id)}
+                                style={{
+                                  backgroundColor: "red",
+                                  color: "white",
+                                }}
+                              >
+                                삭제
+                              </Button>
+                            </li>
+                          ))}
+                        </ul>
+                      ) : (
+                        <p>리뷰가 없습니다</p>
+                      )}
+                    </ModalBody>
+                    <ModalFooter>
+                      <Button
+                        onPress={() => {
+                          if (onReviewEdit) {
+                            onReviewEdit()
+                          }
+                        }}
+                      >
+                        닫기
+                      </Button>
+                    </ModalFooter>
+                  </>
+                )}
+              </>
+            )}
+          </ModalContent>
+        </Modal>
+      </div>
     </div>
   )
 }
