0d8982420aef8a7bbddbf9f7d850225a82613a78
jkeum-dev <jon9eun@gmail.com>
2024-03-06 16:58:25 +0900

    feat : 채팅방 UI 변경, 채팅 상대 정보, 송수신자 메세지 분리, 스크롤

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -3,12 +3,16 @@
 import { useEffect, useState, useRef } from 'react';
 import { Stomp } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
+import { useUser } from '@/hooks/useUser';
+import { useChatRoomInfo } from '@/hooks/useChat';
 
 export default function Chat({ id }) {
 	const [stompClient, setStompClient] = useState(null);
 	const [message, setMessage] = useState('');
 	const [messages, setMessages] = useState([]);
-	const messagesEndRef = useRef(null);
+	const messagesContainerRef = useRef(null);
+	const { user, isLoading, isError } = useUser();
+	const { chatRoom, isChatLoading, isChatError } = useChatRoomInfo(id);
 
 	useEffect(() => {
 		const socket = new SockJS(`${process.env.NEXT_PUBLIC_BASE_URL}/chat`);
@@ -23,35 +27,55 @@ export default function Chat({ id }) {
 		setStompClient(client);
 	}, []);
 
+	useEffect(() => {
+    // 채팅 메시지 컨테이너의 스크롤 높이를 최신 값으로 설정하여 스크롤을 아래로 이동
+    const messagesContainer = messagesContainerRef.current;
+    if (messagesContainer) {
+      messagesContainer.scrollTop = messagesContainer.scrollHeight;
+    }
+  }, [messages]);
+
+	if (isChatLoading || isLoading) {
+		return <div className="h-[60vh] mt-32">loading</div>;
+	}
+
+	if (isChatError || isError) {
+		return <div className="h-[60vh] mt-32">Error: {error.message}</div>;
+	}
+
 	const sendMessage = () => {
 		if (stompClient && stompClient.connected) {
 			const chatMessage = {
 				content: message,
-				type: 'CHAT',
+				sender: user.objData.nickname,
 			};
 			stompClient.send(`/app/chat.sendMessage/${id}`, {}, JSON.stringify(chatMessage));
-			setMessages((prevMessages) => [...prevMessages, chatMessage]);
 			setMessage('');
 		}
-	};
+	}
+
+	const contactTo = user.objData.nickname === chatRoom.objData.userNickname ? chatRoom.objData.hostNickname : chatRoom.objData.userNickname;
 
 	return (
-		<div className="flex flex-col h-[80vh] max-w-2xl mx-auto border border-gray-300 mt-32">
-			<div className="flex-1 overflow-y-auto p-4 space-y-4">
-				{messages.map((msg, idx) => (
-					<div key={idx} className="break-words p-2 rounded-lg bg-blue-100 max-w-xs ml-auto">
-						{msg.content}
-					</div>
-				))}
-				<div ref={messagesEndRef} />
+		<div className="flex flex-col h-[80vh] max-w-2xl mx-auto border border-gray-200 bg-gray-100 mt-32">
+			<div className="p-4 text-lg font-semibold">
+				{/* 이전 페이지로 돌아가기 */}
+				{contactTo}
 			</div>
-			<div className="p-4 border-t border-gray-200 flex items-center">
+			<hr className="border-gray-200" />
+			<div ref={messagesContainerRef} className="messages-container flex-1 overflow-y-auto p-4 space-y-4">
+				{messages.map((msg, idx) => (
+					// 현재 사용자가 보낸 메시지인지 판단하여 ChatMessage 컴포넌트에 전달
+					<ChatMessage key={idx} msg={msg} isCurrentUser={msg.sender === user.objData.nickname} />
+				))}
+			</div>
+			<div className="p-4 border-t border-gray-200 bg-white flex items-center">
 				<input
 					type="text"
 					value={message}
 					onChange={(e) => setMessage(e.target.value)}
-					className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-					placeholder="텍스트를 입력하세요..."
+					className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-base"
+					placeholder="메세지를 입력하세요."
 				/>
 				<button
 					onClick={sendMessage}
@@ -63,3 +87,26 @@ export default function Chat({ id }) {
 		</div>
 	);
 }
+
+const ChatMessage = ({ msg, isCurrentUser }) => {
+	// 현재 사용자가 보낸 메시지인지에 따라 다른 배경색 적용
+	const messageStyle = isCurrentUser
+		? "break-words p-2 rounded-lg bg-blue-100 border border-blue-200 max-w-xs ml-auto text-sm"
+		: "break-words p-2 rounded-lg bg-white border border-gray-200 max-w-xs mr-auto text-sm";
+
+	// timestamp를 Date 객체로 변환
+	const date = new Date(msg.timestamp);
+	// HH:mm 형식으로 시간 변환
+	const formattedTime = date.toLocaleTimeString('ko-KR', {
+		hour: '2-digit',
+		minute: '2-digit',
+		hour12: false // 24시간 형식
+	});
+
+	return (
+		<div className={messageStyle}>
+			<div className="text-xs text-gray-500">{formattedTime}</div>
+			{msg.content}
+		</div>
+	);
+};
---
+++ src/hooks/useChat.js
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "@/config/axios-config";
+
+/**  채팅방 정보 */
+const fetchChatRoomInfo = async (roomId) => {
+	const res = await axios.get(`api/v1/chat/info/${roomId}`,
+		{
+			...axios.defaults,
+			useAuth: true
+		}
+	);
+
+	return res.data;
+};
+
+export const useChatRoomInfo = (roomId) => {
+	const {
+		data: chatRoom,
+		isChatLoading,
+		isFetching,
+		isChatError,
+		error,
+	} = useQuery({
+		queryKey: ["chatRoomInfo", roomId],
+		queryFn: () => fetchChatRoomInfo(roomId),
+	});
+
+	return { chatRoom, isChatLoading, isFetching, isChatError, error };
+};
