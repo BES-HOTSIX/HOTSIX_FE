382d45fbfcbec6e63b123c148eacb4c7cffda057
kyumho kim <js030@github.com>
2024-03-12 17:05:05 +0900

    feat : 숙소 예약 현황 불러오기1

--- src/components/hotel/HotelSales.js
+++ src/components/hotel/HotelSales.js
@@ -9,19 +9,56 @@ import {
   CardFooter,
   Divider,
 } from '@nextui-org/react'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { format, subMonths } from 'date-fns'
+import { ko } from 'date-fns/locale'
+import { fetchHostsHotelReservationDetail } from '@/hooks/useReservation'
+import Pagination from '../Pagination'
+import { FaCheck } from 'react-icons/fa'
+import Link from 'next/link'
 
 export default function HotelSales({ id }) {
   const { hotel, isHotelLoading, isFetching, isError, error } =
     useHotelDetail(id)
 
-  if (isHotelLoading) return <div></div>
+  const [page, setPage] = useState(0)
+  const [totalPages, setTotalPages] = useState(0)
+  const [reservations, setReservations] = useState([])
+  const [currentDate, setCurrentDate] = useState(new Date())
 
-  console.log(hotel)
+  // 오늘 날짜를 'YYYY-MM-DD' 형식의 문자열로 구합니다.
+  const todayStr = new Date().toISOString().split('T')[0]
+
+  useEffect(() => {
+    getReservationsByYearAndMonth(currentDate)
+  }, [currentDate, page]) // 페이지 또는 날짜가 변경될 때마다 실행됩니다.
+
+  const getReservationsByYearAndMonth = async (date) => {
+    const year = format(date, 'yyyy', { locale: ko })
+    const month = format(date, 'M', { locale: ko })
+
+    setCurrentDate(date)
+
+    const hostReservations = await fetchHostsHotelReservationDetail(
+      page,
+      year,
+      month,
+      id
+    )
+      .then((res) => {
+        setReservations(res.objData.content)
+        setTotalPages(res.objData.totalPages)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  if (isHotelLoading) return <div></div>
 
   return (
     <div className='mt-32 min-h-[60vh] text-center'>
-      <div className='grid grid-cols-4'>
+      <div className='grid grid-cols-5'>
         <Card className='col-span-1 py-4 bg-gray-100'>
           <CardHeader className='pb-0 pt-2 px-4 flex-col items-start'>
             <p className='text-tiny uppercase font-bold'>{hotel?.address}</p>
@@ -51,6 +88,81 @@ export default function HotelSales({ id }) {
             </div>
           </CardFooter>
         </Card>
+        <div className='ml-10 col-span-3'>
+          <div className='flex space-x-4'>
+            <button
+              className='bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded'
+              onClick={() => getReservationsByYearAndMonth(new Date())}>
+              {format(new Date(), 'yyyy년 MM월', { locale: ko })}
+            </button>
+            <button
+              className='bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded'
+              onClick={() =>
+                getReservationsByYearAndMonth(subMonths(new Date(), 1))
+              }>
+              {format(subMonths(new Date(), 1), 'yyyy년 MM월', { locale: ko })}
+            </button>
+            <button
+              className='bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded'
+              onClick={() =>
+                getReservationsByYearAndMonth(subMonths(new Date(), 2))
+              }>
+              {format(subMonths(new Date(), 2), 'yyyy년 MM월', { locale: ko })}
+            </button>
+          </div>
+          <div>
+            <div className='overflow-x-auto relative shadow-md sm:rounded-lg'>
+              <table className='mt-6 w-full text-sm text-left text-gray-500'>
+                <thead className='text-xs text-gray-700 uppercase bg-gray-50'>
+                  <tr>
+                    <th scope='col' className='py-3 px-6'>
+                      예약 ID
+                    </th>
+                    <th scope='col' className='py-3 px-6'>
+                      체크인 날짜
+                    </th>
+                    <th scope='col' className='py-3 px-6'>
+                      체크아웃 날짜
+                    </th>
+                    <th scope='col' className='py-3 px-6'>
+                      이름
+                    </th>
+                    <th scope='col' className='py-3 px-6'>
+                      가격
+                    </th>
+                    <th scope='col' className='py-3 px-6'>
+                      이용 완료
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {reservations.map((reservation, index) => (
+                    <tr className='bg-white border-b' key={index}>
+                      <td className='py-4 px-6'>{reservation.id}</td>
+                      <td className='py-4 px-6'>{reservation.checkInDate}</td>
+                      <td className='py-4 px-6'>{reservation.checkOutDate}</td>
+                      <td className='py-4 px-6'>{reservation.buyerName}</td>
+                      <td className='py-4 px-6'>{reservation.paidPrice}</td>
+
+                      <td className='py-4 px-6'>
+                        {' '}
+                        {reservation.checkOutDate < todayStr && <FaCheck />}
+                      </td>
+                      {/* 필요한 다른 데이터 셀 추가 */}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <div className='flex justify-center mt-16'>
+              <Pagination
+                page={page}
+                setPage={setPage}
+                totalPages={totalPages}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
