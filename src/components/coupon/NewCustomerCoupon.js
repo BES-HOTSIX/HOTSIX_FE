db430f5143a66d72b85b3a78e5e176a255022208
kyumho kim <js030@github.com>
2024-03-13 17:05:45 +0900

    feat : 첫 예약 쿠폰 발급 로직 초안 구현

--- src/components/coupon/NewCustomerCoupon.js
+++ src/components/coupon/NewCustomerCoupon.js
@@ -1,8 +1,19 @@
+'use client'
+
 import React from 'react'
 import Image from 'next/image'
 import ForNewCustomer from '@/../public/img/new_customer_coupon.jpg'
+import { useIssueFirstReservationCoupon } from '@/hooks/useCoupon'
 
 export default function NewCustomerCoupon() {
+  const { issueCoupon, isPending, isError, error } =
+    useIssueFirstReservationCoupon()
+
+  const handleIssueFirstReservationCoupon = (e) => {
+    e.preventDefault()
+    issueCoupon()
+  }
+
   return (
     <div className='flex items-center justify-center'>
       <div className='relative mt-11 min-h-[55vh] text-center'>
@@ -12,7 +23,9 @@ export default function NewCustomerCoupon() {
           height={500}
           alt='new-customer-coupon'
         />
-        <button className='absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded'>
+        <button
+          onClick={handleIssueFirstReservationCoupon}
+          className='absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded'>
           발급받기
         </button>
       </div>
