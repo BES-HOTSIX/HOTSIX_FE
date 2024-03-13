17686815449777da4f36c931cfac3acacb735b92
jjy990412 <wjdwn282@gmail.com>
2024-03-13 17:17:35 +0900

    WIP: NearbyAmenitiesMap component update

--- src/components/hotel/NearbyAmenitiesMap.js
+++ src/components/hotel/NearbyAmenitiesMap.js
@@ -1,9 +1,16 @@
 import React, {useEffect, useRef, useState} from "react";
+import {RadioGroup} from "@nextui-org/react";
+import {CustomRadio} from "@/components/ui/CustomRadio";
+import axios from "axios";
 
 export default function NearbyAmenitiesMap({hotel}) {
     const mapRef = useRef(null) // 지도를 표시할 DOM 요소에 대한 참조
     const [map, setMap] = useState(null) // 지도 인스턴스
     const [address, setAddress] = useState(hotel?.address)
+    const [centerCoords, setCenterCoords] = useState()
+    const [nearAmenities, setNearAmenities] = useState([]);
+    const [markers, setMarkers] = useState([]);
+    const [category, setCategory] = useState("food");
 
     useEffect(() => {
         // 네이버 지도 API 스크립트가 이미 로드되었는지 확인
@@ -31,6 +38,8 @@ export default function NearbyAmenitiesMap({hotel}) {
 
                     const result = response.v2.addresses[0]
                     const coords = new naver.maps.LatLng(result?.y, result?.x)
+
+                    setCenterCoords(coords);
 
                     // 지도 중심 이동
                     map.setCenter(coords)
@@ -68,6 +77,52 @@ export default function NearbyAmenitiesMap({hotel}) {
         setMap(createdMap)
     }
 
-    return (<div ref={mapRef} style={{width: '50%', height: '400px'}}>
-    </div>)
+    useEffect(() => {
+        if (category !== "food") {
+            markers.forEach((v) => {
+                v.setMap(null);
+            })
+            setMarkers([])
+        } else if (map && centerCoords) {
+            axios.get(`http://localhost:8080/api/v1/locations/${category}?latitude=${centerCoords.lat()}&longitude=${centerCoords.lng()}&distance=500`).then((r) => {
+                setNearAmenities(r.data);
+            })
+        }
+    }, [centerCoords, category])
+
+    useEffect(() => {
+        if (nearAmenities.length !== 0) {
+            if (markers.length !== 0) {
+                markers.forEach((v) => {
+                    v.setMap(null);
+                })
+                setMarkers([])
+            }
+            nearAmenities.forEach((v) => {
+                let latLng = new naver.maps.LatLng(v.coord.y, v.coord.x);
+
+                markers.push(new naver.maps.Marker({
+                    position: latLng,
+                    title: v.name,
+                    map: map,
+                    icon: {
+                        content: `<div class="border-solid border-2 border-white rounded-md w-11 h-11 overflow-hidden -translate-x-1/2 -translate-y-[130%]">
+                                <img class="w-full h-full object-cover" src="${hotel?.imagesResponse.imageUrl[0]}" alt="marker"/>
+                            </div>`,
+                    },
+                    zIndex: 100
+                }))
+            })
+        }
+    }, [nearAmenities]);
+
+    return (
+        <div ref={mapRef} style={{width: '50%', height: '400px'}}>
+            <RadioGroup className={"z-10"} classNames={{wrapper: ["!flex-row"]}} onValueChange={setCategory}
+                        defaultValue={"food"}>
+                <CustomRadio value={"food"}>음식점</CustomRadio>
+                <CustomRadio value={"cafe"}>카페</CustomRadio>
+            </RadioGroup>
+        </div>
+    )
 }
