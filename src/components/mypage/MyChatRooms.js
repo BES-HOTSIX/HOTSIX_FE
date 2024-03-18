89192042c6335b300fd383e51ed52d60e733967a
jkeum-dev <jon9eun@gmail.com>
2024-03-18 16:35:00 +0900

    fix : 마이페이지 문의내역 탭 삭제

--- src/components/mypage/MyChatRooms.js
+++ src/components/mypage/MyChatRooms.js
@@ -1,163 +1,89 @@
 'use client'
 
 import instance from "@/config/axios-config"
-import { Skeleton, Pagination, Tabs, Tab } from "@nextui-org/react"
+import { Card, CardBody, Skeleton, Pagination } from "@nextui-org/react"
 import { useQuery } from "@tanstack/react-query"
 import { useState, useMemo } from "react"
+import { useRouter } from 'next/navigation'
 import axios from "axios"
-import ChatRoomCard from "../chat/ChatRoomCard"
-import { useUser } from '@/hooks/useUser';
+import { Avatar, Chip } from "@nextui-org/react";
+import { format } from 'date-fns';
 
 export default function MyChatRooms() {
-	const [availablePage, setAvailablePage] = useState(1)
-	const [exitedPage, setExitedPage] = useState(1)
-	const { user, isLoading, isError } = useUser();
+	const [page, setPage] = useState(1)
+	const router = useRouter();
 
-	const chatAvailableQuery = useQuery({
-		queryKey: ["chatAvailable", availablePage],
+	const chatRoomsQuery = useQuery({
+		queryKey: ["chatRooms", page],
 		queryFn: () =>
 			instance
-				.get(`/api/v1/members/me/chatRooms/available?page=${availablePage - 1}`, {
+				.get(`/api/v1/members/me/chatRooms?page=${page - 1}`, {
 					...axios.defaults,
 					useAuth: true,
 				})
 				.then((res) => res.data),
 			keepPreviousData: true,
 	})
-	const { isLoading: isChatAvailableLoading, chatAvailableData, isError: isChatAvailableError, chatAvailableError } = chatAvailableQuery;
 
-	const chatExitedQuery = useQuery({
-		queryKey: ["chatExited", exitedPage],
-		queryFn: () =>
-			instance
-				.get(`/api/v1/members/me/chatRooms/exited?page=${exitedPage - 1}`, {
-					...axios.defaults,
-					useAuth: true,
-				})
-				.then((res) => res.data),
-			keepPreviousData: true,
-	})
-	const { isLoading: isChatExitedLoading, chatExitedData, isError: isChatExitedError, chatExitedError } = chatExitedQuery;
-
-	const availablePages = useMemo(
-		() => chatAvailableQuery.data?.objData.totalPages ?? 0,
-		[chatAvailableQuery.data?.objData.totalPages]
+	const pages = useMemo(
+		() => chatRoomsQuery.data?.objData.totalPages ?? 0,
+		[chatRoomsQuery.data?.objData.totalPages]
 	)
 
-	const exitedPages = useMemo(
-		() => chatExitedQuery.data?.objData.totalPages ?? 0,
-		[chatExitedQuery.data?.objData.totalPages]
-	)
-
-	if (isChatAvailableLoading || isChatExitedLoading || isLoading) {
+	if (chatRoomsQuery.isLoading) {
 		return <div>loading</div>
-	}
-
-	if (isChatAvailableError || isChatExitedError || isError) {
-		return <div>Error</div>
 	}
 
 	return (
 		<div className="flex h-screen">
 			<div className={"flex flex-col w-full gap-5 px-5"}>
-				{user.objData.role === "HOST" &&
-					<Tabs aria-label="Options">
-						<Tab key="진행 중" title="진행 중">
-							{availablePages === 0 && <div>문의내역이 없습니다.</div>}
-							{isChatAvailableLoading &&
-								Array(4)
-									.fill(0)
-										.map((_, index) => {
-											return <Skeleton key={index} height={200}/>
-										})
-							}
-							{chatAvailableQuery.isSuccess &&
-								chatAvailableQuery.data?.objData.content.map((chatRoom, index) => {
-									return (
-										<ChatRoomCard chatRoom={chatRoom} index={index} />
-									)
-								})
-							}
-							{availablePages > 0 ? (
-									<div className='flex w-full justify-center mt-10'>
-										<Pagination
-												isCompact
-												showControls
-												showShadow
-												color='primary'
-												page={availablePage}
-												total={availablePages}
-												onChange={(availablePage) => setAvailablePage(availablePage)}
-										/>
-									</div>
-								) : null
-							}
-						</Tab>
-						<Tab key="종료" title="종료">
-							{exitedPages === 0 && <div>문의내역이 없습니다.</div>}
-							{isChatExitedLoading &&
-								Array(4)
-									.fill(0)
-										.map((_, index) => {
-											return <Skeleton key={index} height={200}/>
-										})
-							}
-							{chatExitedQuery.isSuccess &&
-								chatExitedQuery.data?.objData.content.map((chatRoom, index) => {
-									return (
-										<ChatRoomCard chatRoom={chatRoom} index={index} />
-									)
-								})
-							}
-							{exitedPages > 0 ? (
-									<div className='flex w-full justify-center mt-10'>
-										<Pagination
-												isCompact
-												showControls
-												showShadow
-												color='primary'
-												page={exitedPage}
-												total={exitedPages}
-												onChange={(exitedPage) => setExitedPage(exitedPage)}
-										/>
-									</div>
-								) : null
-							}
-						</Tab>
-					</Tabs>
-				}
-				{user.objData.role === "GUEST" &&
-					<div>
-						{availablePages === 0 && <div>문의내역이 없습니다.</div>}
-						{isChatAvailableLoading &&
-							Array(4)
-								.fill(0)
-									.map((_, index) => {
-										return <Skeleton key={index} height={200}/>
-									})
-						}
-						{chatAvailableQuery.isSuccess &&
-							chatAvailableQuery.data?.objData.content.map((chatRoom, index) => {
-								return (
-									<ChatRoomCard chatRoom={chatRoom} index={index} />
-								)
+				{pages === 0 && <div>문의내역이 없습니다.</div>}
+				{chatRoomsQuery.isLoading &&
+					Array(4)
+						.fill(0)
+							.map((_, index) => {
+								return <Skeleton key={index} height={200}/>
 							})
-						}
-						{availablePages > 0 ? (
-								<div className='flex w-full justify-center mt-10'>
-									<Pagination
-											isCompact
-											showControls
-											showShadow
-											color='primary'
-											page={availablePage}
-											total={availablePages}
-											onChange={(availablePage) => setAvailablePage(availablePage)}
-									/>
-								</div>
-							) : null
-						}
-					</div>
+				}
+				{chatRoomsQuery.isSuccess &&
+					chatRoomsQuery.data?.objData.content.map((chatRoom, index) => {
+						return (
+							<Card key={index} className={"!overflow-visible"} isPressable isHoverable disableAnimation onPress={() => router.push(`/chat/${chatRoom.chatRoomId}`)}>
+								<CardBody>
+									<div className={"flex gap-5"}>
+										<div className={"flex items-center justify-center relative w-1/6 rounded-md overflow-hidden"}>
+												<Avatar src={chatRoom.contactImage} className={"w-24 h-24"} />
+										</div>
+										<div className={"flex flex-col justify-between w-full"}>
+											<div className={"flex justiy-between items-center w-full"}>
+												<div className={"text-2xl"}>{chatRoom.contactNickname}</div>
+												<div className="text-base text-gray-500 ml-auto mr-3">{format(new Date(chatRoom.latestDate), 'MM/dd HH:mm')}</div>
+											</div>
+											<div className={"flex justiy-end items-center w-full"}>
+												{chatRoom.left &&
+													<Chip className="ml-auto mr-3" size="md" color="danger">채팅 종료</Chip>
+												}
+											</div>
+										</div>
+									</div>
+								</CardBody>
+							</Card>
+						)
+					})
+				}
+				{pages > 0 ? (
+						<div className='flex w-full justify-center'>
+							<Pagination
+									isCompact
+									showControls
+									showShadow
+									color='primary'
+									page={page}
+									total={pages}
+									onChange={(page) => setPage(page)}
+							/>
+						</div>
+					) : null
 				}
 			</div>
 		</div>
